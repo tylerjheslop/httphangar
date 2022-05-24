23e134ec9afec2a217fb5380911e8fed0c813dda
Tyler <tylerjheslop@gmail.com>
2022-05-24 03:54:20 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import {Counter} from './features/counter/Counter';
+import {Section, Container, Heading, Image, Hero} from 'react-bulma-components';
 import './App.css';
+import 'bulma/css/bulma.min.css';
+import {config, useSpring, animated} from "react-spring";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+export function Text() {
+    const [flip, set] = useState(false)
+    const props = useSpring({
+        to: {opacity: 1},
+        from: {opacity: 0},
+        reset: true,
+        reverse: flip,
+        delay: 200,
+        config: config.molasses,
+        // onRest: () => set(!flip),
+    })
+    // eslint-disable-next-line react/jsx-no-undef
+    return <animated.h1 style={props}>httphangar</animated.h1>
+}
+
+export function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/*<Container pt={5} breakpoint={'fullhd'}>*/}
+                {/*    <img src={logo} className="App-logo" alt="logo"/>*/}
+                {/*    <h1 className="title"><Text></Text></h1>*/}
+                {/*</Container>*/}
+                <Hero size={'fullheight'}>
+                    <Hero.Header></Hero.Header>
+                    <Hero.Body>
+                        <Container breakpoint={'fullhd'}>
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <br/>
+                            <br/>
+                            <h1 className="title"><Text></Text></h1>
+                        </Container>
+                    </Hero.Body>
+                    <Hero.Footer></Hero.Footer>
+                </Hero>
+
+            </header>
+            <Section>
+                {/*<Heading>Websites</Heading>*/}
+            </Section>
+        </div>
+    );
 }
 
 export default App;
